72f7457e1e9cac92e6fb18765cfda6162ebeed4d
Xander Wayne <xandernzaywa@gmail.com>
2026-07-06 12:42:21 +0300

    feat(auth): create the choose-path screen

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,7 @@ export const Images = {
   sale: require("./gifs/sale.png"),
   analytics: require("./gifs/offline.png"),
   inventory: require("./gifs/stock.png"),
-  zelWordBlack: require("./zelshop-wording-black.svg")
+  zelWordBlack: require("./zelshop-wording-black.svg"),
+  createShop: require("./create-shop.svg"),
+    joinShop: require('./join-shop.svg')
 };
